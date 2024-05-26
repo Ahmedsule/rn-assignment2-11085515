4c783c43d1ccdcbf03a5501baa753dc3480df4bd
smahmed004@st.ug.edu.gh <smahmed004@st.ug.edu>
2024-05-26 23:28:32 +0800

    My first react native app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rn-assignment2-11085515
+![my-app](<WhatsApp Image 2024-05-26 at 23.21.19_8c9630c2.jpg>)
+# creating a new react native blank template project using expo-cli
---
+++ ahmed11085515/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>My name is <Text style={styles.headingspan}>Ahmed Suleman Musbahu</Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading:{
+    fontSize:24
+  },
+  headingspan:{
+    fontWeight: 'bold'
+  }
+});
